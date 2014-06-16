8a4c6affa6fdf02054a034bf76497266d5af98be
Mykel Kochenderfer <mykel@stanford.edu>
2014-06-16 14:14:18 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # TikzPictures
+
+This library allows one to create Tikz pictures and save in various formats. It integrates with IJulia, outputting SVG images to the notebook.
+
+In order to use this library, lualatex must be installed. The texlive and miktex distributions include lualatex. You must also have dvisvgm installed. On Ubuntu, you can get these, if not already present, by running `sudo apt-get install texlive-latex-base` and `sudo apt-get install texlive-binaries`.
+
+## Example
+```julia
+using TikzPictures
+tp = TikzPicture("scale=0.2", "\\draw (0,0) -- (10,10);\n\\draw (10,0) -- (0,10);\n\\node at (5,5) {Mykel Kochenderfer};")
+save(PDF("test"), tp)
+save(SVG("test"), tp)
+save(TEX("test"), tp)
+```
--- src/TikzPictures.jl
+++ src/TikzPictures.jl
@@ -1,5 +1,66 @@
 module TikzPictures
 
-# package code goes here
+export TikzPicture, PDF, TEX, SVG, save
+
+type TikzPicture
+  options::String
+  data::String
+end
+
+type PDF
+  filename::String
+end
+
+type TEX
+  filename::String
+end
+
+type SVG
+  filename::String
+end
+
+Base.mimewritable(::MIME"image/svg+xml", tp::TikzPicture) = true
+
+function save(f::TEX, tp::TikzPicture)
+  filename = f.filename
+  tex = open("$filename", "w")
+  println(tex, "\\documentclass[tikz]{standalone}")
+  println(tex, "\\begin{document}")
+  println(tex, "\\begin{tikzpicture}[")
+  println(tex, tp.options)
+  println(tex, "]")
+  println(tex, tp.data)
+  println(tex, "\\end{tikzpicture}")
+  println(tex, "\\end{document}")
+  close(tex)
+end
+
+function save(f::PDF, tp::TikzPicture)
+  filename = f.filename
+  save(TEX(filename * ".tex"), tp)
+  success(`lualatex $filename`)
+  rm("$filename.tex")
+  rm("$filename.aux")
+  rm("$filename.log")
+end
+
+function save(f::SVG, tp::TikzPicture)
+  filename = f.filename
+  save(TEX(filename * ".tex"), tp)
+  success(`lualatex --output-format=dvi $filename`)
+  success(`dvisvgm --no-fonts $filename`)
+  rm("$filename.tex")
+  rm("$filename.aux")
+  rm("$filename.dvi")
+  rm("$filename.log")
+end
+
+function Base.writemime(f::IO, ::MIME"image/svg+xml", tp::TikzPicture)
+  filename = "tikzpicture"
+  save(SVG(filename), tp)
+  s = readall("$filename.svg")
+  println(f, s)
+  rm("$filename.svg")
+end
 
 end # module
