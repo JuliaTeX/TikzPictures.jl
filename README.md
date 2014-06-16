cd7897786aea3ef62e2ad1eed585c5c73d371aef
Mykel Kochenderfer <mykel@stanford.edu>
2014-06-16 14:55:11 -0700

    Added constructor for tikzpicture

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In order to use this library, lualatex must be installed. The texlive and miktex
 ## Example
 ```julia
 using TikzPictures
-tp = TikzPicture("scale=0.2", "\\draw (0,0) -- (10,10);\n\\draw (10,0) -- (0,10);\n\\node at (5,5) {Mykel Kochenderfer};")
+tp = TikzPicture("\\draw (0,0) -- (10,10);\n\\draw (10,0) -- (0,10);\n\\node at (5,5) {tikz};", options="scale=0.25", preamble="")
 save(PDF("test"), tp)
 save(SVG("test"), tp)
 save(TEX("test"), tp)
--- src/TikzPictures.jl
+++ src/TikzPictures.jl
@@ -3,8 +3,10 @@ module TikzPictures
 export TikzPicture, PDF, TEX, SVG, save
 
 type TikzPicture
-  options::String
   data::String
+  options::String
+  preamble::String
+  TikzPicture(data::String; options="", preamble="") = new(data, options, preamble)
 end
 
 type PDF
@@ -25,6 +27,7 @@ function save(f::TEX, tp::TikzPicture)
   filename = f.filename
   tex = open("$filename", "w")
   println(tex, "\\documentclass[tikz]{standalone}")
+  println(tex, tp.preamble)
   println(tex, "\\begin{document}")
   println(tex, "\\begin{tikzpicture}[")
   println(tex, tp.options)
@@ -36,23 +39,31 @@ function save(f::TEX, tp::TikzPicture)
 end
 
 function save(f::PDF, tp::TikzPicture)
-  filename = f.filename
-  save(TEX(filename * ".tex"), tp)
-  success(`lualatex $filename`)
-  rm("$filename.tex")
-  rm("$filename.aux")
-  rm("$filename.log")
+  try
+    filename = f.filename
+    save(TEX(filename * ".tex"), tp)
+    success(`lualatex $filename`)
+    rm("$filename.tex")
+    rm("$filename.aux")
+    rm("$filename.log")
+  catch
+    error("Error saving as PDF.")
+  end
 end
 
 function save(f::SVG, tp::TikzPicture)
-  filename = f.filename
-  save(TEX(filename * ".tex"), tp)
-  success(`lualatex --output-format=dvi $filename`)
-  success(`dvisvgm --no-fonts $filename`)
-  rm("$filename.tex")
-  rm("$filename.aux")
-  rm("$filename.dvi")
-  rm("$filename.log")
+  try
+    filename = f.filename
+    save(TEX(filename * ".tex"), tp)
+    success(`lualatex --output-format=dvi $filename`)
+    success(`dvisvgm --no-fonts $filename`)
+    rm("$filename.tex")
+    rm("$filename.aux")
+    rm("$filename.dvi")
+    rm("$filename.log")
+  catch
+    error("Error saving as SVG")
+  end
 end
 
 function Base.writemime(f::IO, ::MIME"image/svg+xml", tp::TikzPicture)
