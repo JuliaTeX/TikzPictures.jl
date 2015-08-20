f78bef1a7f94b2a75fa6909a2c2dd704c81d3ef1
Mykel Kochenderfer <mykel@stanford.edu>
2015-08-20 16:10:33 -0700

    improve latex error messages

--- src/TikzPictures.jl
+++ src/TikzPictures.jl
@@ -8,224 +8,241 @@ export LaTeXString, @L_str, @L_mstr
 _tikzDeleteIntermediate = true
 
 function tikzDeleteIntermediate(value::Bool)
-  global _tikzDeleteIntermediate
-  _tikzDeleteIntermediate = value
-  nothing
+    global _tikzDeleteIntermediate
+    _tikzDeleteIntermediate = value
+    nothing
 end
 
 function tikzDeleteIntermediate()
-  global _tikzDeleteIntermediate
-  _tikzDeleteIntermediate
+    global _tikzDeleteIntermediate
+    _tikzDeleteIntermediate
 end
 
 type TikzPicture
-  data::String
-  options::String
-  preamble::String
-  usePDF2SVG::Bool
-  enableWrite18::Bool
-  TikzPicture(data::String; options="", preamble="", usePDF2SVG=true, enableWrite18=true) = new(data, options, preamble, usePDF2SVG, enableWrite18)
+    data::String
+    options::String
+    preamble::String
+    usePDF2SVG::Bool
+    enableWrite18::Bool
+    TikzPicture(data::String; options="", preamble="", usePDF2SVG=true, enableWrite18=true) = new(data, options, preamble, usePDF2SVG, enableWrite18)
 end
 
 type TikzDocument
-  pictures::Vector{TikzPicture}
-  captions::Vector{ASCIIString}
+    pictures::Vector{TikzPicture}
+    captions::Vector{ASCIIString}
 end
 
 TikzDocument() = TikzDocument(TikzPicture[], ASCIIString[])
 
 function push!(td::TikzDocument, tp::TikzPicture; caption="")
-  push!(td.pictures, tp)
-  push!(td.captions, caption)
+    push!(td.pictures, tp)
+    push!(td.captions, caption)
 end
 
 function removeExtension(filename::String, extension::String)
-  if endswith(filename, extension) || endswith(filename, uppercase(extension))
-    return filename[1:(end - length(extension))]
-  else
-    return filename
-  end
+    if endswith(filename, extension) || endswith(filename, uppercase(extension))
+        return filename[1:(end - length(extension))]
+    else
+        return filename
+    end
 end
 
 type PDF
-  filename::String
-  PDF(filename::String) = new(removeExtension(filename, ".pdf"))
+    filename::String
+    PDF(filename::String) = new(removeExtension(filename, ".pdf"))
 end
 
 type TEX
-  filename::String
-  TEX(filename::String) = new(removeExtension(filename, ".tex"))
+    filename::String
+    TEX(filename::String) = new(removeExtension(filename, ".tex"))
 end
 
 type SVG
-  filename::String
-  SVG(filename::String) = new(removeExtension(filename, ".svg"))
+    filename::String
+    SVG(filename::String) = new(removeExtension(filename, ".svg"))
 end
 
 Base.mimewritable(::MIME"image/svg+xml", tp::TikzPicture) = true
 
 function save(f::TEX, tp::TikzPicture)
-  filename = f.filename
-  tex = open("$(filename).tex", "w")
-  println(tex, "\\documentclass[tikz]{standalone}")
-  println(tex, tp.preamble)
-  println(tex, "\\begin{document}")
-  print(tex, "\\begin{tikzpicture}[")
-  print(tex, tp.options)
-  println(tex, "]")
-  println(tex, tp.data)
-  println(tex, "\\end{tikzpicture}")
-  println(tex, "\\end{document}")
-  close(tex)
-end
-
-function save(f::TEX, td::TikzDocument)
-  if isempty(td.pictures)
-    error("TikzDocument does not contain pictures")
-  end
-  filename = f.filename
-  tex = open("$(filename).tex", "w")
-  println(tex, "\\documentclass{article}")
-  println(tex, "\\usepackage{caption}")
-  println(tex, "\\usepackage{tikz}")
-  println(tex, td.pictures[1].preamble)
-  println(tex, "\\begin{document}")
-  println(tex, "\\centering")
-  @assert length(td.pictures) == length(td.captions)
-  i = 1
-  for tp in td.pictures
-    println(tex, "\\centering")
+    filename = f.filename
+    tex = open("$(filename).tex", "w")
+    println(tex, "\\documentclass[tikz]{standalone}")
+    println(tex, tp.preamble)
+    println(tex, "\\begin{document}")
     print(tex, "\\begin{tikzpicture}[")
     print(tex, tp.options)
     println(tex, "]")
     println(tex, tp.data)
     println(tex, "\\end{tikzpicture}")
-    print(tex, "\\captionof{figure}{")
-    print(tex, td.captions[i])
-    println(tex, "}")
-    println(tex, "\\vspace{5ex}")
-    println(tex)
-    i += 1
-  end
-  println(tex, "\\end{document}")
-  close(tex)
+    println(tex, "\\end{document}")
+    close(tex)
+end
+
+function save(f::TEX, td::TikzDocument)
+    if isempty(td.pictures)
+        error("TikzDocument does not contain pictures")
+    end
+    filename = f.filename
+    tex = open("$(filename).tex", "w")
+    println(tex, "\\documentclass{article}")
+    println(tex, "\\usepackage{caption}")
+    println(tex, "\\usepackage{tikz}")
+    println(tex, td.pictures[1].preamble)
+    println(tex, "\\begin{document}")
+    println(tex, "\\centering")
+    @assert length(td.pictures) == length(td.captions)
+    i = 1
+    for tp in td.pictures
+        println(tex, "\\centering")
+        print(tex, "\\begin{tikzpicture}[")
+        print(tex, tp.options)
+        println(tex, "]")
+        println(tex, tp.data)
+        println(tex, "\\end{tikzpicture}")
+        print(tex, "\\captionof{figure}{")
+        print(tex, td.captions[i])
+        println(tex, "}")
+        println(tex, "\\vspace{5ex}")
+        println(tex)
+        i += 1
+    end
+    println(tex, "\\end{document}")
+    close(tex)
+end
+
+function latexerrormsg(s)
+    beginError = false
+    for l in split(s, '\n')
+        if beginError
+            if !isempty(l) && l[1] == '?'
+                return
+            else
+                println(l)
+            end
+        else
+            if !isempty(l) && l[1] == '!'
+                println(l)
+                beginError = true
+            end
+        end
+    end
 end
 
 function save(f::PDF, tp::TikzPicture)
-  # Generate the .tex file and make pass along any possible errors
-  IX = rsearch(f.filename,'/')              # Find the last slash
-  foldername = f.filename[1:IX-1]           # Everything before that is the folder name
-  foldername = (IX == 0 ? "." : foldername) # If there was no slash, folder is "."
-  save(TEX(f.filename * ".tex"), tp)        # Save the tex file in the directory that was given
-                                            #   This will throw an error if the directory doesn't exist
+    # Generate the .tex file and make pass along any possible errors
+    IX = rsearch(f.filename,'/')              # Find the last slash
+    foldername = f.filename[1:IX-1]           # Everything before that is the folder name
+    foldername = (IX == 0 ? "." : foldername) # If there was no slash, folder is "."
+    save(TEX(f.filename * ".tex"), tp)        # Save the tex file in the directory that was given
+    #   This will throw an error if the directory doesn't exist
 
-  # From the .tex file, generate a pdf within the specified folder
-  latexCommand = ``
-  if tp.enableWrite18
-    latexCommand = `lualatex --enable-write18 --output-directory=$(foldername) $(f.filename)`
-  else
-    latexCommand = `lualatex --output-directory=$(foldername) $(f.filename)`
-  end
-  isPdfThere = success(latexCommand)
-
-  if !isPdfThere
-    line1 = "ERROR: The pdf generation failed.\n"
-    line2 = "   Be sure your latex libraries are fully up to date!\n"
-    line3 = "   You tried: $(latexCommand)"
-    error(line1 * line2 * line3)  # Throw an error
-  end
-
-  try
-    # Shouldn't need to be try-catched anymore, but best to be safe
-    # This failing is NOT critical either, so just make it a warning
-    if tikzDeleteIntermediate()
-      rm("$(f.filename).tex")
-      rm("$(f.filename).aux")
-      rm("$(f.filename).log")
+    # From the .tex file, generate a pdf within the specified folder
+    latexCommand = ``
+    if tp.enableWrite18
+        latexCommand = `lualatex --enable-write18 --output-directory=$(foldername) $(f.filename)`
+    else
+        latexCommand = `lualatex --output-directory=$(foldername) $(f.filename)`
     end
-  catch
-    println("WARNING! Your intermediate files are not being deleted.")
-  end
+    latexSuccess = success(latexCommand)
+
+    if !latexSuccess
+        s = readall("$(f.filename).log")
+        latexerrormsg(s)
+        error("LaTeX error")
+    end
+
+    try
+        # Shouldn't need to be try-catched anymore, but best to be safe
+        # This failing is NOT critical either, so just make it a warning
+        if tikzDeleteIntermediate()
+            rm("$(f.filename).tex")
+            rm("$(f.filename).aux")
+            rm("$(f.filename).log")
+        end
+    catch
+        println("WARNING! Your intermediate files are not being deleted.")
+    end
 end
 
 function save(f::PDF, td::TikzDocument)
-  if isempty(td.pictures)
-    error("TikzDocument does not contain pictures")
-  end
-  try
-    filename = f.filename
-    save(TEX(filename * ".tex"), td)
-    if td.pictures[1].enableWrite18
-      success(`lualatex --enable-write18 $filename`)
-    else
-      success(`lualatex $filename`)
+    if isempty(td.pictures)
+        error("TikzDocument does not contain pictures")
     end
+    try
+        filename = f.filename
+        save(TEX(filename * ".tex"), td)
+        if td.pictures[1].enableWrite18
+            success(`lualatex --enable-write18 $filename`)
+        else
+            success(`lualatex $filename`)
+        end
 
-    dir_name,base_name = splitdir(filename)
-    mv("$(base_name).pdf","$filename.pdf")
+        dir_name,base_name = splitdir(filename)
+        mv("$(base_name).pdf","$filename.pdf")
 
-    if tikzDeleteIntermediate()
-      rm("$filename.tex")
-      rm("$(base_name).aux")
-      rm("$(base_name).log")
+        if tikzDeleteIntermediate()
+            rm("$filename.tex")
+            rm("$(base_name).aux")
+            rm("$(base_name).log")
+        end
+    catch
+        println("Error saving as PDF.")
+        rethrow()
     end
-  catch
-    println("Error saving as PDF.")
-    rethrow()
-  end
 end
 
 
 function save(f::SVG, tp::TikzPicture)
-  try
-    filename = f.filename
-    if tp.usePDF2SVG
-      save(PDF(filename), tp)
-      success(`pdf2svg $filename.pdf $filename.svg`)
-      if tikzDeleteIntermediate()
-        rm("$filename.pdf")
-      end
-    else
-      save(TEX("$filename.tex"), tp)
-      if tp.enableWrite18
-        success(`lualatex --enable-write18 --output-format=dvi $filename`)
-      else
-        success(`lualatex --output-format=dvi $filename`)
-      end
-      success(`dvisvgm --no-fonts $filename`)
-      if tikzDeleteIntermediate()
-        rm("$filename.tex")
-        rm("$filename.aux")
-        rm("$filename.dvi")
-        rm("$filename.log")
-      end
+    try
+        filename = f.filename
+        if tp.usePDF2SVG
+            save(PDF(filename), tp)
+            success(`pdf2svg $filename.pdf $filename.svg`) || error("pdf2svg failure")
+            if tikzDeleteIntermediate()
+                rm("$filename.pdf")
+            end
+        else
+            save(TEX("$filename.tex"), tp)
+            if tp.enableWrite18
+                success(`lualatex --enable-write18 --output-format=dvi $filename`)
+            else
+                success(`lualatex --output-format=dvi $filename`)
+            end
+            success(`dvisvgm --no-fonts $filename`)
+            if tikzDeleteIntermediate()
+                rm("$filename.tex")
+                rm("$filename.aux")
+                rm("$filename.dvi")
+                rm("$filename.log")
+            end
+        end
+    catch
+        println("Error saving as SVG")
+        rethrow()
     end
-  catch
-    println("Error saving as SVG")
-    rethrow()
-  end
 end
 
 # this is needed to work with multiple images in ijulia (kind of a hack)
 global _tikzid = round(Uint64, time() * 1e6)
 
 function Base.writemime(f::IO, ::MIME"image/svg+xml", tp::TikzPicture)
-  global _tikzid
-  filename = "tikzpicture"
-  save(SVG(filename), tp)
-  s = readall("$filename.svg")
-  s = replace(s, "glyph", "glyph-$(_tikzid)-")
-  s = replace(s, "\"clip", "\"clip-$(_tikzid)-")
-  s = replace(s, "#clip", "#clip-$(_tikzid)-")
-  s = replace(s, "\"image", "\"image-$(_tikzid)-")
-  s = replace(s, "#image", "#image-$(_tikzid)-")
-  s = replace(s, "linearGradient id=\"linear", "linearGradient id=\"linear-$(_tikzid)-")
-  s = replace(s, "#linear", "#linear-$(_tikzid)-")
-  _tikzid += 1
-  println(f, s)
-  if tikzDeleteIntermediate()
-    rm("$filename.svg")
-  end
+    global _tikzid
+    filename = "tikzpicture"
+    save(SVG(filename), tp)
+    s = readall("$filename.svg")
+    s = replace(s, "glyph", "glyph-$(_tikzid)-")
+    s = replace(s, "\"clip", "\"clip-$(_tikzid)-")
+    s = replace(s, "#clip", "#clip-$(_tikzid)-")
+    s = replace(s, "\"image", "\"image-$(_tikzid)-")
+    s = replace(s, "#image", "#image-$(_tikzid)-")
+    s = replace(s, "linearGradient id=\"linear", "linearGradient id=\"linear-$(_tikzid)-")
+    s = replace(s, "#linear", "#linear-$(_tikzid)-")
+    _tikzid += 1
+    println(f, s)
+    if tikzDeleteIntermediate()
+        rm("$filename.svg")
+    end
 end
 
 end # module
