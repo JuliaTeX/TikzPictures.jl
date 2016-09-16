6c6f7727177e2102321d74ecef1d1c4e60ae72f1
Mykel Kochenderfer <mykel@stanford.edu>
2016-09-15 22:08:11 -0700

    only test lualatex compile if lualatex exists

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using TikzPictures
 using Base.Test
 
-@assert success(`lualatex -v`)
 using TikzPictures
 tp = TikzPicture("\\draw (0,0) -- (10,10);\n\\draw (10,0) -- (0,10);\n\\node at (5,5) {tikz \$\\sqrt{\\pi}\$};", options="scale=0.25", preamble="")
-save(PDF("test"), tp)
-save(SVG("test"), tp)
 save(TEX("test"), tp)
+if success(`lualatex -v`)
+    save(PDF("test"), tp)
+    save(SVG("test"), tp)
+end
