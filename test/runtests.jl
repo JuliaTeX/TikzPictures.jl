f9d7cc009e1b7c50ad1dc0e93834ee1a2965cb3b
sidthekid402 <sidthekid402@gmail.com>
2018-06-21 14:34:53 -0700

    Julia 0.7 updates and intermediate file creation in tmp dirs

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,13 @@ using Test
 
 using TikzPictures
 tp = TikzPicture("\\draw (0,0) -- (10,10);\n\\draw (10,0) -- (0,10);\n\\node at (5,5) {tikz \$\\sqrt{\\pi}\$};", options="scale=0.25", preamble="")
-save(TEX("test"), tp)
+td = TikzDocument([tp],["hello"])
+save(TEX("testPic"), tp)
 if success(`lualatex -v`)
-	save(SVG("test"), tp)
-    save(PDF("test"), tp)
+	save(PDF("testPic"), tp)
+    save(SVG("testPic"), tp)
+    save(PDF("testDoc"), td)
 end
 
-tp = TikzPicture(L"""
-\draw (0,0) -- (10,10);
-\draw (10,0) -- (0,10);
-\node at (5,5) {tikz $\sqrt{\pi}$};"""
-, options="scale=0.25", preamble="")
 
-if success(`lualatex -v`)
-	save(SVG("test"), tp)
-    save(PDF("test"), tp)
-end
+
